0a92b9a6ff25b789975fe6e2b9a4d8d8fe7aabb9
1515571@qq.com <8>
2018-01-25 15:18:21 +0800

    springbean工厂容器

--- step-1-container-register-and-get/src/test/java/cn/titan/test/UserService.java
+++ step-1-container-register-and-get/src/test/java/cn/titan/test/UserService.java
@@ -2,6 +2,6 @@ package cn.titan.test;
 
 public class UserService {
 	public void helloworld(String name) {
-		System.out.println(name + ":hello world");
+		System.out.println(name + " : hello world");
 	}
 }
